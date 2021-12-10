59f18b945df6dba281ee333ee7b3a0cdb2e4a49e
satonohime <nananariru@gmail.com>
2021-12-10 12:30:58 -0800

    add return object when submit is clicked, responsive design in progress

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,15 @@ function switch_theme(event) {
 toggleButton = document.querySelector('.toggle')
 
 toggleButton.addEventListener('click', switch_theme)
+
+
+document.querySelector('#frm-btn').addEventListener('click', (evt) => {
+    evt.preventDefault()
+    let obj = {}
+    
+    obj.name = document.querySelector('#name').value
+    obj.email = document.querySelector('#email').value
+    obj.message = document.querySelector('#msg').value
+
+    console.log(obj)
+})
